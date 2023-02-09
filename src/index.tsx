c350f20c144a9096985b7ab661e9a5d33bd1d3e0
vinny <vinny.vip07@gmail.com>
2023-02-09 14:36:14 -0300

    feat: inserting provider and ToastFy container

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ToastContainer } from 'react-toastify';
 import App from './shared/App';
+import { Provider } from './shared/ContextApi/Provider';
 import './shared/Global.scss';
 
 const root = ReactDOM.createRoot(
@@ -8,6 +10,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ToastContainer />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
