6b02843258b5c72f337b85715435cd6a0d9d374e
vinny <vinny.vip07@gmail.com>
2023-02-09 14:37:59 -0300

    feat: delete file

--- src/modules/Home/Service/DeletePeople.ts
+++
@@ -1,7 +0,0 @@
-import { People } from '../domain/interfaces/IPeople';
-
-export function DeletePeople(peoples: People[], index: number) {
-  const newPeoples = peoples.splice(index, 1);
-
-  localStorage.setItem('peoples', JSON.stringify(newPeoples));
-}
